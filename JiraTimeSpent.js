e3fa3eb8440f8e422e32c7e393f09df88841cff2
Ксения Платова <k.platova@superjob.ru>
2017-02-20 16:55:53 +0300

    Обновление скприта

--- JiraTimeSpent.js
+++ JiraTimeSpent.js
@@ -1,73 +1,108 @@
 // ==UserScript==
-// @name         Jira TimeSpent
+// @name         Jira Time Issues Calculator
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://jira.superjob.ru/issues*
-// @grant        none
+// @description  Плагин считает "Первоначальная оценка" и "Затраченное время" в выдаче тасков по какому-либо запросу (эти колонки должны присутствовать)
+// @author       you
+// @match        https://jira.domain.ru/issues*
+// @grant        
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var TOTAL_ESTIMATE_ID = 'aggregatetimeoriginalestimate';
-    var TIME_SPENT_ID = 'timespent';
+    $('.navigator-body').bind('DOMSubtreeModified', addRowsWithCalculatedTime);
 
-    var colLength = $('.rowHeader th').length;
+    addRowsWithCalculatedTime();
 
-    var totalEctimateColIndex = calculateColIndex(TOTAL_ESTIMATE_ID);
-    var timeSpentColIndex = calculateColIndex(TIME_SPENT_ID);
+    function addRowsWithCalculatedTime(){
+        var TOTAL_ESTIMATE_ID = 'aggregatetimeoriginalestimate';
+        var TIME_SPENT_ID = 'timespent';
+        var TIME_CALCULATED_CLASS_NAME = 'sj-time-calculated';
 
-    var totalEstimate = 0;
-    var timeSpent = 0;
+        var $rows = $('.' + TIME_CALCULATED_CLASS_NAME);
+        var rowsLength = $rows.length;
 
-    var totalEstimateCol = '';
-    var timeSpentCol = '';
-    var emptyCol = '<td></td>';
-    var cols = '';
-    var currentCol = '';
-
-    for (var i = 0; i < colLength; i++) {
-        switch (i) {
-            case totalEctimateColIndex:
-                currentCol = makeCol(calculateTime('aggregatetimeoriginalestimate', totalEstimate));
-                break;
-            case timeSpentColIndex:
-                currentCol = makeCol(calculateTime('timespent', timeSpent));
-                break;
-            default:
-                currentCol = emptyCol;
+        if (rowsLength) {
+            return false;
         }
-        cols += currentCol;
+
+        var colLength = $('.rowHeader th').length;
+
+        var totalEctimateColIndex = calculateColIndex(TOTAL_ESTIMATE_ID);
+        var timeSpentColIndex = calculateColIndex(TIME_SPENT_ID);
+
+        var totalEstimate = 0;
+        var timeSpent = 0;
+
+        var totalEstimateCol = '';
+        var timeSpentCol = '';
+        var emptyCol = '<td></td>';
+        var cols = '';
+        var currentCol = '';
+
+        for (var i = 0; i < colLength; i++) {
+            switch (i) {
+                case totalEctimateColIndex:
+                    currentCol = makeCol(calculateTime('aggregatetimeoriginalestimate', totalEstimate));
+                    break;
+                case timeSpentColIndex:
+                    currentCol = makeCol(calculateTime('timespent', timeSpent));
+                    break;
+                default:
+                    currentCol = emptyCol;
+            }
+            cols += currentCol;
+        }
+
+        cols = '<tr class="' + TIME_CALCULATED_CLASS_NAME + '">' + cols + '</tr>';
+
+        $('#issuetable tbody').append(cols).prepend(cols);
+
+        function calculateColIndex(id) {
+            var index = null;
+            $('.rowHeader th').each(function(i){
+                index = ($(this).attr('data-id') === id) ? i : index;
+            });
+            return index;
+        }
+
+        function makeCol(value) {
+            return '<td>' + value + 'ч</td>';
+        }
+
+        function calculateTime(id, value) {
+            var b = value;
+            $('.' + id).each(function(i){
+
+                var timeDimension = $(this).text().slice(-1);
+                var a = ($(this).text()) ? parseFloat($(this).text().replace(/н|д|ч|м|с/g, '').replace(',', '.')) : 0;
+
+                switch (timeDimension) {
+                    case 'н':
+                        a = a * 24 * 7;
+                        break;
+                    case 'д':
+                        a = a * 24;
+                        break;
+                    case 'ч':
+                        break;
+                    case 'м':
+                        a = 1 / 60 * a;
+                        break;
+                    case 'с':
+                        a = 1 / 60 / 60 * a;
+                        break;
+                    default:
+
+                }
+
+                b += a;
+
+            });
+            return b.toFixed(2);
+        }
     }
 
-    cols = '<tr>' + cols + '</tr>';
-
-    $('#issuetable tbody').append(cols).prepend(cols);
-
-    function calculateColIndex(id) {
-        var index = null;
-        $('.rowHeader th').each(function(i){
-            index = ($(this).attr('data-id') === id) ? i : index;
-        });
-        return index;
-    }
-
-    function makeCol(value) {
-        return '<td>' + value + 'ч</td>';
-    }
-
-    function calculateTime(id, value) {
-        var b = value;
-        $('.' + id).each(function(i){
-
-            var a = ($(this).text()) ? parseFloat($(this).text().replace('ч', '').replace(',', '.')) : 0;
-
-            b += a;
-
-        });
-        return b;
-    }
 
 })();
